9ed521ffe3d3da29e34cc32d026fbf425ec378a2
VAN-max96 <vedant_s23@mtu.ac.in>
2026-07-13 11:10:41 +0530

    Ai test 2

--- vedant_soibam/ai_api.py
+++ vedant_soibam/ai_api.py
@@ -11,10 +11,10 @@ from openai import OpenAI
 
 # 1. NVIDIA API configuration
 BASE_URL = "https://integrate.api.nvidia.com/v1"
-API_KEY =( "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")  # Put your newly generated key here!
+API_KEY = "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"  # Put your newly generated key here!
 
 # 2. Setup FastAPI
-app = FastAPI(title="Nexus AI API - NVIDIA Powered")
+app = FastAPI(title="VEDIC AI - NVIDIA Powered")
 client = OpenAI(api_key=API_KEY, base_url=BASE_URL)
 
 app.add_middleware(
@@ -46,7 +46,12 @@ def ai_response(message: ChatMessage):
         messages_array = [
             {
                 "role": "system", 
-                "content": f"You are Nexus, a premium AI assistant. The user you are talking to is named Vedant. Always be helpful, friendly, and concise. The current date and time is {current_time_string} .My iam name is vedant iam originally fom malom tulihal mainipur ,iam a software engineer and iam studying b.tech in a university called MTU(Maniput Technical University). I am a very good person and i love to help people. I am very friendly and i love to make new friends. I am very concious about my work and i always try to do my best. I am very passionate about my work and i always try to learn new things. I am very creative and i always try to think out of the box. I am very positive and i always try to see the good in people. I am very humble and i always try to be kind to everyone. I am very honest and i always try to be truthful. I am very loyal and i always try to be faithful. I am very responsible and i always try to be accountable. I am very respectful and i always try to be polite. I am very patient and i always try to be understanding. I am very generous and i always try to be giving. I am very compassionate and i always try to be empathetic. I am very courageous and i always try to be brave. I am very determined and i always try to be persistent. I am very disciplined and i always try to be self-controlled. I am very focused and i always try to be attentive. I am very organized and i always try to be systematic. I am very resourceful and i always try to be innovative. I am very adaptable and i always try to be flexible. I am very resilient and i always try to be strong."
+                "content": (
+                    f"You are an AI assistant. The user you are talking to is named VEDANT SOIBAM. "
+                    f"He is currently pursuing a B.Tech course at Manipur Technical University in Computer Science Engineering. "
+                    f"He is currently an intern at Cubeten, Mantripukhri for AI/ML, working on developing model projects. "
+                    f"Always be helpful, friendly, and concise. The current date and time is {current_time_string}."
+                )
             }
         ]
 
